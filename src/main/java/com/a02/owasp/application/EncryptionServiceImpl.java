c778ba52bdd2232fc0bfcb88134af818ac08dc65
Lenin <lenin.changotasig@cobiscorp.com>
2024-12-28 02:50:46 -0500

    Ajustes de encriptación

--- src/main/java/com/a02/owasp/application/EncryptionServiceImpl.java
+++ src/main/java/com/a02/owasp/application/EncryptionServiceImpl.java
@@ -3,19 +3,32 @@ package com.a02.owasp.application;
 import com.a02.owasp.domain.ports.in.EncryptionService;
 
 import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 import java.util.Base64;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class EncryptionServiceImpl implements EncryptionService {
-    private static final String ALGORITHM = "AES";
-    private static final String SECRET_KEY = "1234567890123456";
+    private static final String ALGORITHM = "AES/GCM/NoPadding";
+    //private static final String SECRET_KEY = "1234567890123456";
 
     @Override
     public String encrypt(String data) throws Exception {
+        // Se obtiene la fecha y hora del sistema
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String formattedDateTime = now.format(formatter);
+        // Se genera un key aleatorio
+        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+        keyGenerator.init(128); // Puedes usar 192 o 256 bits también
+        SecretKey secretKey = keyGenerator.generateKey();
         Cipher cipher = Cipher.getInstance(ALGORITHM);
-        SecretKeySpec key = new SecretKeySpec(SECRET_KEY.getBytes(), ALGORITHM);
+        SecretKeySpec key = new SecretKeySpec(secretKey.getEncoded(), ALGORITHM);
         cipher.init(Cipher.ENCRYPT_MODE, key);
         byte[] encrypted = cipher.doFinal(data.getBytes());
+        System.out.println("Fecha y hora del sistema: " + formattedDateTime);
         return Base64.getEncoder().encodeToString(encrypted);
     }
 
